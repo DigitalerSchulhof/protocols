7a167ebf88f1933d261931a91aa4bcad3dda2b3b
Jesper Engberg <jeengbe@gmail.com>
2023-06-17 18:40:04 +0200

    dont include sources

--- clients/ts/build.sh
+++ clients/ts/build.sh
@@ -3,8 +3,8 @@ set -e
 PROTOS_PATH=$(realpath "$PWD/../../proto")
 PROTOC_GEN_TS_PATH="$PWD/node_modules/.bin/protoc-gen-ts"
 
-rm -rf ./build
-mkdir -p ./build/src
+rm -rf ./build ./src
+mkdir -p ./build ./src
 
 find $PROTOS_PATH \
   -name '*.proto' \
@@ -12,10 +12,10 @@ find $PROTOS_PATH \
     grpc_tools_node_protoc \
       --proto_path $PROTOS_PATH \
       --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-      --ts_out=service=grpc-node,mode=grpc-js:./build/src \
+      --ts_out=service=grpc-node,mode=grpc-js:./src \
       --ts_opt=no_namespace,unary_rpc_promise=true \
       {} \;
 
 tsc
 
-cp ./package.json ./build/package.json
+cp ./package.json.build ./build/package.json
